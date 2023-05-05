ff5e7e57d1503e0590d4fff6c151744aea15881f
Paula Tamires <paulatamiress@outlook.com>
2023-05-05 15:15:02 -0300

    Content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,32 @@
+export const dataFake = [
+    {
+        'id':'1',        
+        'title': 'Marvel Studios',
+        'description': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus consectetur molestiae veniam, asperiores inventore veritatis provident quae sit error quasi earum laborum dicta dolorem at illum. Maiores temporibus id tempore.',
+        'photoCover' : 'https://disneyplusbrasil.com.br/wp-content/uploads/2023/04/Universo-Cinematografico-da-Marvel.jpg'
+    },
+
+    {
+        'id':'2',        
+        'title': 'Natasha Romanoff',
+        'description': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus consectetur molestiae veniam, asperiores inventore veritatis provident quae sit error quasi earum laborum dicta dolorem at illum. Maiores temporibus id tempore.',
+        'photoCover' : 'https://www.gaz.com.br/uploads/2021/07/viuva-negra-filme-marvel-scarlet-johannson.jpg'
+    },
+
+    {
+        'id':'3',        
+        'title': 'Yelena Belova',
+        'description': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus consectetur molestiae veniam, asperiores inventore veritatis provident quae sit error quasi earum laborum dicta dolorem at illum. Maiores temporibus id tempore.',
+        'photoCover' : 'https://t.ctcdn.com.br/0PUZ1srJkq8GzA7vI0fJ6QdBmyY=/512x288/smart/filters:format(webp)/i461800.jpeg'
+    },
+
+    {
+        'id':'4',        
+        'title': 'Steve Rogers',
+        'description': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus consectetur molestiae veniam, asperiores inventore veritatis provident quae sit error quasi earum laborum dicta dolorem at illum. Maiores temporibus id tempore.',
+        'photoCover' : 'https://media-assets.wired.it/photos/615f10bf0f2b3dd590a02651/master/w_1600%2Cc_limit/1472206765_Chris-Evans-Captain-America-Trilogy.jpg'
+    }
+
+    
+]
+
